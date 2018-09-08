96e374277ce9a9b7855da105c265582579665add
Truls Hjertaas <thjertaas@csumb.edu>
2018-09-08 23:56:41 +0000

    Ferdig nesttside

--- labs/lab2/777/index.php
+++ labs/lab2/777/index.php
@@ -4,6 +4,31 @@
         <title> 777 Slot Machine </title>
     </head>
     <body>
-
+        <?php
+        
+        function victory() {
+        
+        $random_img = rand(0,3);
+        
+        if ($random_img == 0) {
+            $symbol = "cherry";
+        } else if ($random_img == 1) {
+            $symbol = "grapes";
+        } else if ($random_img == 2) {
+            $symbol = "orange";
+        } else if ($random_img == 3) {
+            $symbol = "lemon";
+        }
+        
+        
+        echo "<img src='img/$symbol.png' alt='Cherry' title='Cherry'/>";
+        
+        };
+        
+        victory();
+        victory();
+        victory();
+        
+        ?>
     </body>
 </html>
