951d80505e384db9781e735ed8b75a2e963b2637
Truls Hjertaas <thjertaas@csumb.edu>
2018-09-14 03:46:10 +0000

    Slotmachine done

---
+++ labs/lab2/777/inc/functions.php
@@ -0,0 +1,46 @@
+<?php 
+        
+        function play() {
+        for($i=1; $i<4; $i++){
+            ${"random_img" .$i} = rand(0,2);
+            victory(${"random_img" .$i});
+        }
+        }
+        
+        displayPoints($random_img1, $random_img2, $random_img3);
+
+        function victory($random_img) {
+        
+        switch($random_img) {
+            case 0: $symbol = "cherry";
+            break;
+            case 1: $symbol = "seven";
+            break;
+            case 2: $symbol = "lemon";
+            break;
+        }
+        echo "<img src='img/$symbol.png' alt='$symbol' title='".ucfirst($symbol)."' width='200'>";
+        
+        
+        }
+        
+        function displayPoints($random_img1, $random_img2, $random_img3) {
+            echo"<section id='output'>";
+            if($random_img1 == $random_img2 && $random_img2 == $random_img3) {
+                switch($random_img1) {
+                    case 0: $totalPoints = 1000;
+                            echo "<h1>Jackpot!</h1>";
+                            break;
+                    case 1: $totalPoints = 500;
+                            break;
+                    case 2: $totalPoints = 250;
+                            break;
+                }
+                echo "<h2>You won $totalPoints points!</h2>";
+            } else {
+                echo "<h3>Better luck next time!</h3>";
+            }
+            echo "</section>";
+        }
+
+?>
--- labs/lab2/777/index.php
+++ labs/lab2/777/index.php
@@ -2,58 +2,66 @@
 <html>
     <head>
         <title> 777 Slot Machine </title>
+        
+        <style type="text/css">
+            @import url("css/styles.css");
+        </style>
+        
     </head>
     <body>
-        <? php 
         
-        $random_img1 = rand(0,2);
-        victory($random_img1);
-        $random_img2 = rand(0,2);
-        victory($random_img2);
-        $random_img3 = rand(0,2);
-        victory($random_img3);
+        <div id="main">
         
-        /*for($i=1; $i<4; $i++){
+        <?php 
+        for($i=1; $i<4; $i++){
             ${"random_img" .$i} = rand(0,2);
-            victory(${"random_img" .$i});
-        }*/
+            victory(${"random_img" .$i}, $i);
+        }
         
         displayPoints($random_img1, $random_img2, $random_img3);
 
-        function victory($random_img) {
+        function victory($random_img, $pos) {
         
         switch($random_img) {
-            case 0: $symbol = "cherry";
+            case 0: $symbol = "seven";
             break;
-            case 1: $symbol = "seven";
+            case 1: $symbol = "cherry";
             break;
             case 2: $symbol = "lemon";
             break;
         }
-        echo "<img src=img/$symbol.png' alt='$symbol' title=\"".ucfirst($symbol)."\"/>";
-        
+        echo "<img id='reel$pos' src='img/$symbol.png' alt='$symbol' title='".ucfirst($symbol)."' width='70px'>";
         
         }
         
         function displayPoints($random_img1, $random_img2, $random_img3) {
-            echo"<section id='output'>";
+            echo"<div id='output'>";
             if($random_img1 == $random_img2 && $random_img2 == $random_img3) {
                 switch($random_img1) {
                     case 0: $totalPoints = 1000;
                             echo "<h1>Jackpot!</h1>";
+                            echo '<EMBED SRC="tada.mp3" HIDDEN="TRUE" AUTOSTART="TRUE"></EMBED>';
                             break;
                     case 1: $totalPoints = 500;
+                            echo '<EMBED SRC="tada.mp3" HIDDEN="TRUE" AUTOSTART="TRUE"></EMBED>';
                             break;
                     case 2: $totalPoints = 250;
+                            echo '<EMBED SRC="tada.mp3" HIDDEN="TRUE" AUTOSTART="TRUE"></EMBED>';
                             break;
                 }
-                echo "<h2> You won'$totalPoints  points!</h2>";
+                echo "<h2>You won $totalPoints points!</h2>";
             } else {
                 echo "<h3>Better luck next time!</h3>";
             }
-            echo "</section>";
+            echo "</div>";
         }
 
-?>
+        ?>
+        
+        <form>
+            <input type="submit" value="Spin!"/>
+        </form>
+        
+        </div>
     </body>
 </html>
